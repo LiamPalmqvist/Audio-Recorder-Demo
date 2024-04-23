80fd8d0cd453562953af801a6307ac65ce7e091e
Liam Palmqvist <Liam.palmqvist@icloud.com>
2024-04-23 19:16:07 +0100

    NAudio -> CSCore + Dual audio recording started

--- Audio Recorder Demo/MainWindow.xaml.cs
+++ Audio Recorder Demo/MainWindow.xaml.cs
@@ -12,8 +12,15 @@ using System.Diagnostics;
 using Microsoft.Win32;
 using System.Runtime.InteropServices;
 using System.Data.Common;
-using NAudio;
-using NAudio.Wave;
+//using NAudio;
+//using NAudio.Wave;
+using CSCore;
+using CSCore.SoundIn;
+using CSCore.Streams;
+using CSCore.Codecs.WAV;
+using CSCore.CoreAudioAPI;
+
+
 
 namespace Audio_Recorder_Demo
 {   
@@ -22,11 +29,17 @@ namespace Audio_Recorder_Demo
 
         internal string RecordingURI = "";
         string tempName = System.IO.Path.GetTempFileName();
-  
-        internal WaveInEvent waveIn = new WaveInEvent();
-        internal WasapiLoopbackCapture desktopWaveIn = new WasapiLoopbackCapture(); 
-        internal WaveFileWriter writer = null;
-        internal WaveFileWriter desktopWriter = null;
+
+        // Set up global audio capture sources
+        internal WaveIn? soundKeeper = null;
+        internal WasapiLoopbackCapture? desktopSoundKeeper = null;
+        internal IWriteable ?audioWriter = null;
+        internal IWaveSource ?finalWaveSource = null;
+        internal IWaveSource ?desktopFinalWaveSource = null;
+
+        // Setting up Threading
+        Thread micThread;
+        Thread desktopThread;
 
         public MainWindow()
         {
@@ -38,12 +51,24 @@ namespace Audio_Recorder_Demo
             AudioRecorderButton.Visibility = Visibility.Collapsed;
             AudioRecorderOffButton.Visibility = Visibility.Visible;
 
-            writer = new WaveFileWriter(tempName, waveIn.WaveFormat);
-            waveIn.StartRecording();
-            waveIn.DataAvailable += (s, a) =>
+            soundKeeper = new WaveIn();
+            //soundKeeper.Device = selected_device;
+            soundKeeper.Initialize();
+            var soundInSource = new SoundInSource(soundKeeper);
+
+            var singleBlockNotificationStream = new SingleBlockNotificationStream(soundInSource.ToSampleSource());
+            finalWaveSource = singleBlockNotificationStream.ToWaveSource();
+
+            audioWriter = new WaveWriter(tempName, finalWaveSource.WaveFormat);
+            byte[] buffer = new byte[finalWaveSource.WaveFormat.BytesPerSecond / 2];
+            soundInSource.DataAvailable += (s, e) =>
             {
-                writer.Write(a.Buffer, 0, a.BytesRecorded);
+                int read;
+                while ((read = finalWaveSource.Read(buffer, 0, buffer.Length)) > 0)
+                    audioWriter.Write(buffer, 0, read);
             };
+
+            soundKeeper.Start();
 
             URITextBlock.Text = "Recording!!";
         }
@@ -58,14 +83,17 @@ namespace Audio_Recorder_Demo
             dialogue.FilterIndex = 2; //this just tells the computer which dialogue
             // to select the second dialogue option
 
-            waveIn.StopRecording();
+            // These won't be null because the button cannot be pressed if
+            // the software isn't recording
+            soundKeeper.Stop();
+            soundKeeper.Dispose();
+            soundKeeper = null;
+            finalWaveSource.Dispose();
 
-            waveIn.RecordingStopped += (s, a) =>
+            if (audioWriter is IDisposable)
             {
-                writer?.Dispose();
-                writer = null;
-                waveIn.Dispose();
-            };
+                ((IDisposable)audioWriter).Dispose();
+            }
 
             // This saves the file after the recording is done and put in a temp file
             if (dialogue.ShowDialog() == true)
@@ -90,12 +118,24 @@ namespace Audio_Recorder_Demo
             DesktopAudioRecorderButton.Visibility = Visibility.Collapsed;
             DesktopAudioRecorderOffButton.Visibility = Visibility.Visible;
 
-            desktopWriter = new WaveFileWriter(tempName, desktopWaveIn.WaveFormat);
-            desktopWaveIn.StartRecording();
-            desktopWaveIn.DataAvailable += (s, a) =>
+            desktopSoundKeeper = new WasapiLoopbackCapture();
+            //soundKeeper.Device = selected_device;
+            desktopSoundKeeper.Initialize();
+            var soundInSource = new SoundInSource(desktopSoundKeeper);
+
+            var singleBlockNotificationStream = new SingleBlockNotificationStream(soundInSource.ToSampleSource());
+            finalWaveSource = singleBlockNotificationStream.ToWaveSource();
+
+            audioWriter = new WaveWriter(tempName, finalWaveSource.WaveFormat);
+            byte[] buffer = new byte[finalWaveSource.WaveFormat.BytesPerSecond / 2];
+            soundInSource.DataAvailable += (s, e) =>
             {
-                desktopWriter.Write(a.Buffer, 0, a.BytesRecorded);
+                int read;
+                while ((read = finalWaveSource.Read(buffer, 0, buffer.Length)) > 0)
+                    audioWriter.Write(buffer, 0, read);
             };
+
+            desktopSoundKeeper.Start();
 
             URITextBlock.Text = "Recording!!";
         }
@@ -105,19 +145,23 @@ namespace Audio_Recorder_Demo
             DesktopAudioRecorderOffButton.Visibility = Visibility.Collapsed;
             DesktopAudioRecorderButton.Visibility = Visibility.Visible;
 
+            // These won't be null because the button cannot be pressed if
+            // the software isn't recording
+            desktopSoundKeeper.Stop();
+            desktopSoundKeeper.Dispose();
+            desktopSoundKeeper = null;
+            finalWaveSource.Dispose();
+
+            if (audioWriter is IDisposable)
+            {
+                ((IDisposable)audioWriter).Dispose();
+            }
+
+            Trace.WriteLine("Success");
+
             var dialogue = new SaveFileDialog();
             dialogue.Filter = "mp3 (*.mp3)|*.mp3|wav (.wav)|*.wav|All files (*.*)|*.*";
             dialogue.FilterIndex = 2; //this just tells the computer which dialogue
-            // to select the second dialogue option
-
-            desktopWaveIn.StopRecording();
-
-            desktopWaveIn.RecordingStopped += (s, a) =>
-            {
-                desktopWriter?.Dispose();
-                desktopWriter = null;
-                desktopWaveIn.Dispose();
-            };
 
             // This saves the file after the recording is done and put in a temp file
             if (dialogue.ShowDialog() == true)
@@ -141,12 +185,18 @@ namespace Audio_Recorder_Demo
         {
             RecordAllButton.Visibility = Visibility.Collapsed;
             StopRecordAllButton.Visibility = Visibility.Visible;
+
+            StartDesktopRecording();
+            StartMicRecording();
+
         }
 
         private void StopRecordAllButton_Click(object sender, RoutedEventArgs e)
         {
             StopRecordAllButton.Visibility = Visibility.Collapsed;
             RecordAllButton.Visibility = Visibility.Visible;
+
+
         }
 
         private void RecordAllSwitch_Click(object sender, RoutedEventArgs e)
@@ -163,6 +213,58 @@ namespace Audio_Recorder_Demo
             DesktopAudioRecorderOffButton.Visibility = Visibility.Visible;
             AudioRecorderButton.Visibility = Visibility.Visible;
             AudioRecorderOffButton.Visibility = Visibility.Visible;
+        }
+        
+        async Task StartDesktopRecording()
+        {
+            await Task.Run(() =>
+            { 
+                desktopSoundKeeper = new WasapiLoopbackCapture();
+                desktopSoundKeeper.Initialize();
+
+                var desktopSoundInSource = new SoundInSource(desktopSoundKeeper);
+
+                var desktopSingleBlockNotificationStream = new SingleBlockNotificationStream(desktopSoundInSource.ToSampleSource());
+                desktopFinalWaveSource = desktopSingleBlockNotificationStream.ToWaveSource();
+
+                audioWriter = new WaveWriter(System.IO.Path.GetTempFileName(), desktopFinalWaveSource.WaveFormat);
+                byte[] buffer = new byte[finalWaveSource.WaveFormat.BytesPerSecond / 2];
+
+                desktopSoundInSource.DataAvailable += (s, e) =>
+                {
+                    int read;
+                    while ((read = desktopFinalWaveSource.Read(buffer, 0, buffer.Length)) > 0)
+                        audioWriter.Write(buffer, 0, (int)read);
+                };
+
+                desktopSoundKeeper.Start();
+            });
+        }
+
+        async Task StartMicRecording()
+        {
+            await Task.Run(() =>
+            {
+                soundKeeper = new WaveIn();
+
+                soundKeeper.Initialize();
+
+                var micSoundInSource = new SoundInSource(soundKeeper);
+
+                var micSingleBlockNotificationStream = new SingleBlockNotificationStream(micSoundInSource.ToSampleSource());
+                finalWaveSource = micSingleBlockNotificationStream.ToWaveSource();
+
+                audioWriter = new WaveWriter(System.IO.Path.GetTempFileName(), finalWaveSource.WaveFormat);
+                byte[] buffer = new byte[finalWaveSource.WaveFormat.BytesPerSecond / 2];
+                micSoundInSource.DataAvailable += (s, e) =>
+                {
+                    int read;
+                    while ((read = finalWaveSource.Read(buffer, 0, buffer.Length)) > 0)
+                        audioWriter.Write(buffer, 0, read);
+                };
+
+                soundKeeper.Start();
+            });
         }
     }
 }
