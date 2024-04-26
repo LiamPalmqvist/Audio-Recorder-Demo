e5958cb52314f3de5cc7d8d2c2996975678bed15
Liam Palmqvist <Liam.palmqvist@icloud.com>
2024-04-26 23:38:14 +0100

    Because of the way Wasapi works, when you record system audio it doesn't record silence.

--- Audio Recorder Demo/MainWindow.xaml.cs
+++ Audio Recorder Demo/MainWindow.xaml.cs
@@ -20,6 +20,8 @@ using CSCore.Streams;
 using CSCore.Codecs.WAV;
 using CSCore.CoreAudioAPI;
 using System.IO;
+using NAudio.Wave.SampleProviders;
+using CSCore.SoundOut;
 
 
 
@@ -42,10 +44,6 @@ namespace Audio_Recorder_Demo
 
         // set up for cancellation of tasks
         CancellationTokenSource tokenSource = new CancellationTokenSource();
-
-        // Setting up Threading
-        Thread micThread;
-        Thread desktopThread;
 
         public MainWindow()
         {
@@ -285,53 +283,9 @@ namespace Audio_Recorder_Demo
                 {
                     RecordingURI2 += URI;
                 }
-                
-
-                Trace.WriteLine(RecordingURI2);
-
-
-
-                FileInfo file = new FileInfo(tempName2);
-                try
-                {
-                    using (FileStream stream = file.OpenRead())
-                    {
-                        stream.Close();
-                    }
-                }
-                catch (Exception ex)
-                {
-                    foreach (Process process in FileUtil.WhoIsLocking(tempName2))
-                    {
-                        Trace.WriteLine("processname: " + process);
-                        process.Close();
-                    }
-                }
-                
-                /*
-                file = new FileInfo(tempName2);
-                try
-                {
-                    using (FileStream stream = file.OpenRead())
-                    {
-                        stream.Close();
-                    }
-                }
-                catch (Exception ex)
-                {
-                    foreach (Process process in FileUtil.WhoIsLocking(tempName))
-                    {
-                        Trace.WriteLine("processname: " + process);
-                        process.Close();
-                    }
-                }
-                */
-
-                
-                System.IO.File.Move(tempName, RecordingURI, true);
-                
+             
+                System.IO.File.Move(tempName, RecordingURI, true);  
                 System.IO.File.Move(tempName2, RecordingURI2, true);
-
             }
 
         }
@@ -343,151 +297,13 @@ namespace Audio_Recorder_Demo
             AudioRecorderButton.Visibility = Visibility.Collapsed;
             AudioRecorderOffButton.Visibility = Visibility.Collapsed;
         }
-        
+
         private void RecordAllSwitch_Unclick(object sender, RoutedEventArgs e)
         {
             DesktopAudioRecorderButton.Visibility = Visibility.Visible;
             DesktopAudioRecorderOffButton.Visibility = Visibility.Visible;
             AudioRecorderButton.Visibility = Visibility.Visible;
             AudioRecorderOffButton.Visibility = Visibility.Visible;
-        }
-
-        public static class FileUtil
-        {
-            [StructLayout(LayoutKind.Sequential)]
-            struct RM_UNIQUE_PROCESS
-            {
-                public int dwProcessId;
-                public System.Runtime.InteropServices.ComTypes.FILETIME ProcessStartTime;
-            }
-
-            const int RmRebootReasonNone = 0;
-            const int CCH_RM_MAX_APP_NAME = 255;
-            const int CCH_RM_MAX_SVC_NAME = 63;
-
-            enum RM_APP_TYPE
-            {
-                RmUnknownApp = 0,
-                RmMainWindow = 1,
-                RmOtherWindow = 2,
-                RmService = 3,
-                RmExplorer = 4,
-                RmConsole = 5,
-                RmCritical = 1000
-            }
-
-            [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
-            struct RM_PROCESS_INFO
-            {
-                public RM_UNIQUE_PROCESS Process;
-
-                [MarshalAs(UnmanagedType.ByValTStr, SizeConst = CCH_RM_MAX_APP_NAME + 1)]
-                public string strAppName;
-
-                [MarshalAs(UnmanagedType.ByValTStr, SizeConst = CCH_RM_MAX_SVC_NAME + 1)]
-                public string strServiceShortName;
-
-                public RM_APP_TYPE ApplicationType;
-                public uint AppStatus;
-                public uint TSSessionId;
-                [MarshalAs(UnmanagedType.Bool)]
-                public bool bRestartable;
-            }
-
-            [DllImport("rstrtmgr.dll", CharSet = CharSet.Unicode)]
-            static extern int RmRegisterResources(uint pSessionHandle,
-                                                  UInt32 nFiles,
-                                                  string[] rgsFilenames,
-                                                  UInt32 nApplications,
-                                                  [In] RM_UNIQUE_PROCESS[] rgApplications,
-                                                  UInt32 nServices,
-                                                  string[] rgsServiceNames);
-
-            [DllImport("rstrtmgr.dll", CharSet = CharSet.Auto)]
-            static extern int RmStartSession(out uint pSessionHandle, int dwSessionFlags, string strSessionKey);
-
-            [DllImport("rstrtmgr.dll")]
-            static extern int RmEndSession(uint pSessionHandle);
-
-            [DllImport("rstrtmgr.dll")]
-            static extern int RmGetList(uint dwSessionHandle,
-                                        out uint pnProcInfoNeeded,
-                                        ref uint pnProcInfo,
-                                        [In, Out] RM_PROCESS_INFO[] rgAffectedApps,
-                                        ref uint lpdwRebootReasons);
-
-            /// <summary>
-            /// Find out what process(es) have a lock on the specified file.
-            /// </summary>
-            /// <param name="path">Path of the file.</param>
-            /// <returns>Processes locking the file</returns>
-            /// <remarks>See also:
-            /// http://msdn.microsoft.com/en-us/library/windows/desktop/aa373661(v=vs.85).aspx
-            /// http://wyupdate.googlecode.com/svn-history/r401/trunk/frmFilesInUse.cs (no copyright in code at time of viewing)
-            /// 
-            /// </remarks>
-            static public List<Process> WhoIsLocking(string path)
-            {
-                uint handle;
-                string key = Guid.NewGuid().ToString();
-                List<Process> processes = new List<Process>();
-
-                int res = RmStartSession(out handle, 0, key);
-                if (res != 0) throw new Exception("Could not begin restart session.  Unable to determine file locker.");
-
-                try
-                {
-                    const int ERROR_MORE_DATA = 234;
-                    uint pnProcInfoNeeded = 0,
-                         pnProcInfo = 0,
-                         lpdwRebootReasons = RmRebootReasonNone;
-
-                    string[] resources = new string[] { path }; // Just checking on one resource.
-
-                    res = RmRegisterResources(handle, (uint)resources.Length, resources, 0, null, 0, null);
-
-                    if (res != 0) throw new Exception("Could not register resource.");
-
-                    //Note: there's a race condition here -- the first call to RmGetList() returns
-                    //      the total number of process. However, when we call RmGetList() again to get
-                    //      the actual processes this number may have increased.
-                    res = RmGetList(handle, out pnProcInfoNeeded, ref pnProcInfo, null, ref lpdwRebootReasons);
-
-                    if (res == ERROR_MORE_DATA)
-                    {
-                        // Create an array to store the process results
-                        RM_PROCESS_INFO[] processInfo = new RM_PROCESS_INFO[pnProcInfoNeeded];
-                        pnProcInfo = pnProcInfoNeeded;
-
-                        // Get the list
-                        res = RmGetList(handle, out pnProcInfoNeeded, ref pnProcInfo, processInfo, ref lpdwRebootReasons);
-                        if (res == 0)
-                        {
-                            processes = new List<Process>((int)pnProcInfo);
-
-                            // Enumerate all of the results and add them to the 
-                            // list to be returned
-                            for (int i = 0; i < pnProcInfo; i++)
-                            {
-                                try
-                                {
-                                    processes.Add(Process.GetProcessById(processInfo[i].Process.dwProcessId));
-                                }
-                                // catch the error -- in case the process is no longer running
-                                catch (ArgumentException) { }
-                            }
-                        }
-                        else throw new Exception("Could not list processes locking resource.");
-                    }
-                    else if (res != 0) throw new Exception("Could not list processes locking resource. Failed to get size of result.");
-                }
-                finally
-                {
-                    RmEndSession(handle);
-                }
-
-                return processes;
-            }
         }
     }
 }
