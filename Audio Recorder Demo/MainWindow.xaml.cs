5852afaeb4307b5d333c29460289b556bfb8e3d9
Liam Palmqvist <Liam.palmqvist@icloud.com>
2024-04-26 22:47:36 +0100

    Simultanious audio/desktop recording WORKING!

--- Audio Recorder Demo/MainWindow.xaml.cs
+++ Audio Recorder Demo/MainWindow.xaml.cs
@@ -19,6 +19,7 @@ using CSCore.SoundIn;
 using CSCore.Streams;
 using CSCore.Codecs.WAV;
 using CSCore.CoreAudioAPI;
+using System.IO;
 
 
 
@@ -29,13 +30,18 @@ namespace Audio_Recorder_Demo
 
         internal string RecordingURI = "";
         string tempName = System.IO.Path.GetTempFileName();
+        string tempName2 = System.IO.Path.GetTempFileName();
 
         // Set up global audio capture sources
         internal WaveIn? soundKeeper = null;
         internal WasapiLoopbackCapture? desktopSoundKeeper = null;
         internal IWriteable ?audioWriter = null;
+        internal IWriteable ?desktopAudioWriter = null;
         internal IWaveSource ?finalWaveSource = null;
         internal IWaveSource ?desktopFinalWaveSource = null;
+
+        // set up for cancellation of tasks
+        CancellationTokenSource tokenSource = new CancellationTokenSource();
 
         // Setting up Threading
         Thread micThread;
@@ -186,16 +192,147 @@ namespace Audio_Recorder_Demo
             RecordAllButton.Visibility = Visibility.Collapsed;
             StopRecordAllButton.Visibility = Visibility.Visible;
 
-            StartDesktopRecording();
-            StartMicRecording();
+            // Set up for cancellation of task
+            CancellationToken ct = tokenSource.Token;
+            
+            Task.Run(() =>
+            {
+                tempName = System.IO.Path.GetTempFileName();
 
+                soundKeeper = new WaveIn();
+
+                soundKeeper.Initialize();
+
+                var micSoundInSource = new SoundInSource(soundKeeper);
+
+                var micSingleBlockNotificationStream = new SingleBlockNotificationStream(micSoundInSource.ToSampleSource());
+                finalWaveSource = micSingleBlockNotificationStream.ToWaveSource();
+
+                audioWriter = new WaveWriter(tempName, finalWaveSource.WaveFormat);
+                byte[] buffer = new byte[finalWaveSource.WaveFormat.BytesPerSecond / 2];
+                micSoundInSource.DataAvailable += (s, e) =>
+                {
+                    int read;
+                    while ((read = finalWaveSource.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        audioWriter.Write(buffer, 0, read);
+                    }
+                };
+                soundKeeper.Start();
+            }, tokenSource.Token);
+
+            Task.Run(() =>
+            {
+                tempName = System.IO.Path.GetTempFileName();
+
+                desktopSoundKeeper = new WasapiLoopbackCapture();
+                desktopSoundKeeper.Initialize();
+
+                var desktopSoundInSource = new SoundInSource(desktopSoundKeeper);
+
+                var desktopSingleBlockNotificationStream = new SingleBlockNotificationStream(desktopSoundInSource.ToSampleSource());
+                desktopFinalWaveSource = desktopSingleBlockNotificationStream.ToWaveSource();
+
+                desktopAudioWriter = new WaveWriter(tempName2, desktopFinalWaveSource.WaveFormat);
+                byte[] buffer = new byte[desktopFinalWaveSource.WaveFormat.BytesPerSecond / 2];
+
+                desktopSoundInSource.DataAvailable += (s, e) =>
+                {
+                    int read;
+                    while ((read = desktopFinalWaveSource.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        desktopAudioWriter.Write(buffer, 0, (int)read);
+                    }
+                };
+                desktopSoundKeeper.Start();
+            }, tokenSource.Token);
         }
 
-        private void StopRecordAllButton_Click(object sender, RoutedEventArgs e)
+        private async void StopRecordAllButton_Click(object sender, RoutedEventArgs e)
         {
             StopRecordAllButton.Visibility = Visibility.Collapsed;
             RecordAllButton.Visibility = Visibility.Visible;
 
+            soundKeeper.Stop();
+            soundKeeper.Dispose();
+            soundKeeper = null;
+            finalWaveSource.Dispose();
+
+            ((IDisposable)audioWriter).Dispose();
+
+
+            desktopSoundKeeper.Stop();
+            desktopSoundKeeper.Dispose();
+            desktopSoundKeeper = null;
+            desktopFinalWaveSource.Dispose();
+
+            ((IDisposable)desktopAudioWriter).Dispose();
+
+            var dialogue = new SaveFileDialog();
+            dialogue.Filter = "mp3 (*.mp3)|*.mp3|wav (.wav)|*.wav|All files (*.*)|*.*";
+            dialogue.FilterIndex = 2; //this just tells the computer which dialogue
+
+            // This saves the file after the recording is done and put in a temp file
+            if (dialogue.ShowDialog() == true)
+            {
+                RecordingURI = dialogue.FileName.Replace("/", "\\");
+                
+                // Simple temp name for audio recording desktop part
+                string RecordingURI2 = "";
+                string[] temp = RecordingURI.Split(".");
+                temp[temp.Length - 2] += "_Desktop.";
+                foreach (string URI in temp)
+                {
+                    RecordingURI2 += URI;
+                }
+                
+
+                Trace.WriteLine(RecordingURI2);
+
+
+
+                FileInfo file = new FileInfo(tempName2);
+                try
+                {
+                    using (FileStream stream = file.OpenRead())
+                    {
+                        stream.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    foreach (Process process in FileUtil.WhoIsLocking(tempName2))
+                    {
+                        Trace.WriteLine("processname: " + process);
+                        process.Close();
+                    }
+                }
+                
+                /*
+                file = new FileInfo(tempName2);
+                try
+                {
+                    using (FileStream stream = file.OpenRead())
+                    {
+                        stream.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    foreach (Process process in FileUtil.WhoIsLocking(tempName))
+                    {
+                        Trace.WriteLine("processname: " + process);
+                        process.Close();
+                    }
+                }
+                */
+
+                
+                System.IO.File.Move(tempName, RecordingURI, true);
+                
+                System.IO.File.Move(tempName2, RecordingURI2, true);
+
+            }
 
         }
 
@@ -214,57 +351,143 @@ namespace Audio_Recorder_Demo
             AudioRecorderButton.Visibility = Visibility.Visible;
             AudioRecorderOffButton.Visibility = Visibility.Visible;
         }
-        
-        async Task StartDesktopRecording()
+
+        public static class FileUtil
         {
-            await Task.Run(() =>
-            { 
-                desktopSoundKeeper = new WasapiLoopbackCapture();
-                desktopSoundKeeper.Initialize();
-
-                var desktopSoundInSource = new SoundInSource(desktopSoundKeeper);
-
-                var desktopSingleBlockNotificationStream = new SingleBlockNotificationStream(desktopSoundInSource.ToSampleSource());
-                desktopFinalWaveSource = desktopSingleBlockNotificationStream.ToWaveSource();
-
-                audioWriter = new WaveWriter(System.IO.Path.GetTempFileName(), desktopFinalWaveSource.WaveFormat);
-                byte[] buffer = new byte[finalWaveSource.WaveFormat.BytesPerSecond / 2];
-
-                desktopSoundInSource.DataAvailable += (s, e) =>
-                {
-                    int read;
-                    while ((read = desktopFinalWaveSource.Read(buffer, 0, buffer.Length)) > 0)
-                        audioWriter.Write(buffer, 0, (int)read);
-                };
-
-                desktopSoundKeeper.Start();
-            });
-        }
-
-        async Task StartMicRecording()
-        {
-            await Task.Run(() =>
+            [StructLayout(LayoutKind.Sequential)]
+            struct RM_UNIQUE_PROCESS
             {
-                soundKeeper = new WaveIn();
+                public int dwProcessId;
+                public System.Runtime.InteropServices.ComTypes.FILETIME ProcessStartTime;
+            }
 
-                soundKeeper.Initialize();
+            const int RmRebootReasonNone = 0;
+            const int CCH_RM_MAX_APP_NAME = 255;
+            const int CCH_RM_MAX_SVC_NAME = 63;
 
-                var micSoundInSource = new SoundInSource(soundKeeper);
+            enum RM_APP_TYPE
+            {
+                RmUnknownApp = 0,
+                RmMainWindow = 1,
+                RmOtherWindow = 2,
+                RmService = 3,
+                RmExplorer = 4,
+                RmConsole = 5,
+                RmCritical = 1000
+            }
 
-                var micSingleBlockNotificationStream = new SingleBlockNotificationStream(micSoundInSource.ToSampleSource());
-                finalWaveSource = micSingleBlockNotificationStream.ToWaveSource();
+            [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+            struct RM_PROCESS_INFO
+            {
+                public RM_UNIQUE_PROCESS Process;
 
-                audioWriter = new WaveWriter(System.IO.Path.GetTempFileName(), finalWaveSource.WaveFormat);
-                byte[] buffer = new byte[finalWaveSource.WaveFormat.BytesPerSecond / 2];
-                micSoundInSource.DataAvailable += (s, e) =>
+                [MarshalAs(UnmanagedType.ByValTStr, SizeConst = CCH_RM_MAX_APP_NAME + 1)]
+                public string strAppName;
+
+                [MarshalAs(UnmanagedType.ByValTStr, SizeConst = CCH_RM_MAX_SVC_NAME + 1)]
+                public string strServiceShortName;
+
+                public RM_APP_TYPE ApplicationType;
+                public uint AppStatus;
+                public uint TSSessionId;
+                [MarshalAs(UnmanagedType.Bool)]
+                public bool bRestartable;
+            }
+
+            [DllImport("rstrtmgr.dll", CharSet = CharSet.Unicode)]
+            static extern int RmRegisterResources(uint pSessionHandle,
+                                                  UInt32 nFiles,
+                                                  string[] rgsFilenames,
+                                                  UInt32 nApplications,
+                                                  [In] RM_UNIQUE_PROCESS[] rgApplications,
+                                                  UInt32 nServices,
+                                                  string[] rgsServiceNames);
+
+            [DllImport("rstrtmgr.dll", CharSet = CharSet.Auto)]
+            static extern int RmStartSession(out uint pSessionHandle, int dwSessionFlags, string strSessionKey);
+
+            [DllImport("rstrtmgr.dll")]
+            static extern int RmEndSession(uint pSessionHandle);
+
+            [DllImport("rstrtmgr.dll")]
+            static extern int RmGetList(uint dwSessionHandle,
+                                        out uint pnProcInfoNeeded,
+                                        ref uint pnProcInfo,
+                                        [In, Out] RM_PROCESS_INFO[] rgAffectedApps,
+                                        ref uint lpdwRebootReasons);
+
+            /// <summary>
+            /// Find out what process(es) have a lock on the specified file.
+            /// </summary>
+            /// <param name="path">Path of the file.</param>
+            /// <returns>Processes locking the file</returns>
+            /// <remarks>See also:
+            /// http://msdn.microsoft.com/en-us/library/windows/desktop/aa373661(v=vs.85).aspx
+            /// http://wyupdate.googlecode.com/svn-history/r401/trunk/frmFilesInUse.cs (no copyright in code at time of viewing)
+            /// 
+            /// </remarks>
+            static public List<Process> WhoIsLocking(string path)
+            {
+                uint handle;
+                string key = Guid.NewGuid().ToString();
+                List<Process> processes = new List<Process>();
+
+                int res = RmStartSession(out handle, 0, key);
+                if (res != 0) throw new Exception("Could not begin restart session.  Unable to determine file locker.");
+
+                try
                 {
-                    int read;
-                    while ((read = finalWaveSource.Read(buffer, 0, buffer.Length)) > 0)
-                        audioWriter.Write(buffer, 0, read);
-                };
+                    const int ERROR_MORE_DATA = 234;
+                    uint pnProcInfoNeeded = 0,
+                         pnProcInfo = 0,
+                         lpdwRebootReasons = RmRebootReasonNone;
 
-                soundKeeper.Start();
-            });
+                    string[] resources = new string[] { path }; // Just checking on one resource.
+
+                    res = RmRegisterResources(handle, (uint)resources.Length, resources, 0, null, 0, null);
+
+                    if (res != 0) throw new Exception("Could not register resource.");
+
+                    //Note: there's a race condition here -- the first call to RmGetList() returns
+                    //      the total number of process. However, when we call RmGetList() again to get
+                    //      the actual processes this number may have increased.
+                    res = RmGetList(handle, out pnProcInfoNeeded, ref pnProcInfo, null, ref lpdwRebootReasons);
+
+                    if (res == ERROR_MORE_DATA)
+                    {
+                        // Create an array to store the process results
+                        RM_PROCESS_INFO[] processInfo = new RM_PROCESS_INFO[pnProcInfoNeeded];
+                        pnProcInfo = pnProcInfoNeeded;
+
+                        // Get the list
+                        res = RmGetList(handle, out pnProcInfoNeeded, ref pnProcInfo, processInfo, ref lpdwRebootReasons);
+                        if (res == 0)
+                        {
+                            processes = new List<Process>((int)pnProcInfo);
+
+                            // Enumerate all of the results and add them to the 
+                            // list to be returned
+                            for (int i = 0; i < pnProcInfo; i++)
+                            {
+                                try
+                                {
+                                    processes.Add(Process.GetProcessById(processInfo[i].Process.dwProcessId));
+                                }
+                                // catch the error -- in case the process is no longer running
+                                catch (ArgumentException) { }
+                            }
+                        }
+                        else throw new Exception("Could not list processes locking resource.");
+                    }
+                    else if (res != 0) throw new Exception("Could not list processes locking resource. Failed to get size of result.");
+                }
+                finally
+                {
+                    RmEndSession(handle);
+                }
+
+                return processes;
+            }
         }
     }
 }
